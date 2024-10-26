d041cff45a56eab9d8dcb7ee3c90a53bac19e0bd
David Leyva <dleyvaabrahantes@gmail.com>
2024-10-26 15:45:15 -0400

    fix internal

--- Sources/CustomDialog/CustomDialogPackage.swift
+++ Sources/CustomDialog/CustomDialogPackage.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 @available(iOS 16.0.0, *)
 public struct CustomDialogPackage {
-    public static func customDialog(nameImage: String, nameColor: Color, title: String, subtitle: String, actionNo: @escaping () -> Void, actionYes: @escaping () -> Void ){
+    public static func customDialog(nameImage: String, nameColor: Color, title: String, subtitle: String, actionNo: @escaping () -> Void, actionYes: @escaping () -> Void )-> some View {
         ReactangleWithCurvedTopView(nameImage: nameImage, nameColor: nameColor, title: title, subtitle: subtitle, actionNo: actionNo, actionYes: actionYes)
     }
 }
